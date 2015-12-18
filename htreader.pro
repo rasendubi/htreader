7211cf769c2760a7b2bcf61b5437fbbc73e2ab62
Alexey Shmalko <rasen.dubi@gmail.com>
2015-12-18 20:38:47 +0200

    Initial commit

---
+++ htreader.pro
@@ -0,0 +1,5 @@
+TEMPLATE = app
+TARGET = htreader
+INCLUDEPATH += .
+
+SOURCES += src/main.cpp
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+  QLabel label("Hello, world!");
+  label.show();
+  return app.exec();
+}
