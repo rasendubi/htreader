d553808366499c397585295875d5aeefe2639cd2
Oleksandr Sochka <sasha.sochka@gmail.com>
2015-12-20 00:23:39 +0400

    [front-end] random shit

--- public/app/cards.component.ts
+++ public/app/cards.component.ts
@@ -1,22 +1,38 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit, AfterViewInit} from 'angular2/core';
 import {Card} from './card';
 import {CardsService} from './cards.service';
 
 @Component({
-    templateUrl: 'app/cards.component.html'
+    templateUrl: 'app/cards.component.html',
+    styleUrls: [
+        'node_modules/bootstrap/dist/css/bootstrap.css',
+        'app/cards.component.css'
+    ],
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit, AfterViewInit {
     public cards: Card[] = [];
+    public userClickedOk = false;
     constructor(private _cardsService: CardsService) { }
+
     ngOnInit() {
         this._cardsService.getCards().then(cards => this.cards = cards);
+    }
+
+    ngAfterViewInit() {
+        // document.getElementById("ok-btn").focus();
     }
 
     selectedCard() {
         return this.cards[0];
     }
 
+    okClicked() {
+        this.userClickedOk = true;
+        document.getElementById("mark-btn-4").focus();
+    }
+
     setMark(value: Number) {
-        this.cards.splice(0, 1);
+        this.cards.splice(0, 1); // remove this card from the list
+        this.userClickedOk = false;
     }
 }
