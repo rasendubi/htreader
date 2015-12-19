e910e63241fda2f9c1aa2f1dce52256b0dbb9f01
Oleksandr Sochka <sasha.sochka@gmail.com>
2015-12-19 05:59:03 +0400

    add some shit

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -2,6 +2,21 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <div class="row" id="main">
+        <div class="col-md-3" id="leftpanel">
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+            <router-outlet></router-outlet>
+        </div>
+        <div class="col-md-9" id="rightpanel">Right panel</div>
+    </div>
+    `,
+    styleUrls: [
+        'node_modules/bootstrap/dist/css/bootstrap.css',
+        'app/app.component.css'
+    ]
 })
-export class AppComponent { }
+export class AppComponent { 
+    
+}
