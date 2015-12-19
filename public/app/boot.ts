be12b4fa89f7b672f7e3ae3e62472b63e590edd0
Oleksandr Sochka <sasha.sochka@gmail.com>
2015-12-20 02:04:59 +0400

    [front-end] use http.get for list of cards

--- public/app/boot.ts
+++ public/app/boot.ts
@@ -1,13 +1,15 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import {AppComponent} from './app.component';
 import {TextsService} from './texts.service';
 import {ExtractsService} from './extracts.service';
 import {CardsService} from './cards.service';
 
 bootstrap(AppComponent, [
-    ROUTER_PROVIDERS, 
-    TextsService, 
-    ExtractsService, 
+    ROUTER_PROVIDERS,
+    HTTP_PROVIDERS,
+    TextsService,
+    ExtractsService,
     CardsService
 ]);
--- public/app/cards.service.ts
+++ public/app/cards.service.ts
@@ -1,23 +1,14 @@
-import {Component} from 'angular2/core';
-import {Injectable} from 'angular2/core';
+import {Component, Injectable} from 'angular2/core';
+import {Http} from "angular2/http";
 import {Card} from './card'
 
 @Injectable()
 export class CardsService {
+    constructor(private _http: Http) { }
     getCards() {
-        return Promise.resolve([
-            <Card> {id: 1, answer: "Answer", question: "Ques3tion"},
-            <Card> {id: 2, answer: "Ans222wer", question: "Ques33tio 23n"},
-            <Card> {id: 3, answer: "Ans2wer", question: "Quest2ion"},
-            <Card> {id: 4, answer: "Answ22er", question: "Ques3t3ion"},
-            <Card> {id: 5, answer: "Answer", question: "Quest33ion"},
-            <Card> {id: 6, answer: "Answ2er", question: "Ques3t3ion"},
-            <Card> {id: 7, answer: "Answ2er", question: "Quest33ion"},
-            <Card> {id: 8, answer: "Answer", question: "Quest3ion"},
-            <Card> {id: 9, answer: "Answer", question: "Quest33ion"},
-            <Card> {id: 10, answer: "Ans2wer", question: "Ques33tion"},
-            <Card> {id: 11, answer: "Ans2wer", question: "Ques3tion"}
-        ]);
+        return new Promise((resolve, reject) => {
+            this._http.get('/api/cards').subscribe(value => resolve(<Card> value.json()));
+        });
     }
 }
 
