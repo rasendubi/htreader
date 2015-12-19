a0636ac365912944a8edc3aaec8279b482882edc
Oleksandr Sochka <sasha.sochka@gmail.com>
2015-12-20 02:08:37 +0400

    [front-end] export enableProdMode

--- public/app/boot.ts
+++ public/app/boot.ts
@@ -1,10 +1,13 @@
-import {bootstrap}    from 'angular2/platform/browser';
+import {bootstrap} from 'angular2/platform/browser';
+import {enableProdMode} from 'angular2/core';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {AppComponent} from './app.component';
 import {TextsService} from './texts.service';
 import {ExtractsService} from './extracts.service';
 import {CardsService} from './cards.service';
+
+// enableProdMode();
 
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
